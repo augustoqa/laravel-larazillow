d7fe8994216134d8c9985181f1a4598d3f480995
Augusto Quisquinay <augustoacual@gmail.com>
2024-03-22 18:34:31 -0600

    Listing Details NonProp Attributes

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -3,7 +3,8 @@
     <Box v-for="listing in listings" :key="listing.id">
       <div>
         <Link :href="route('listing.show', { listing: listing.id })">
-          <ListingAddress :listing="listing" />
+          <listing-space :listing="listing"></listing-space>
+          <ListingAddress :listing="listing" class="text-gray-500" />
         </Link>
       </div>
       <div>
@@ -24,6 +25,7 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
 import ListingAddress from '@/components/ListingAddress.vue'
+import ListingSpace from '@/components/ListingSpace.vue'
 import Box from '@/components/UI/Box.vue'
 
 defineProps({

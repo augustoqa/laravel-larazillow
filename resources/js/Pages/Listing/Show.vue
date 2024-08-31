77dbd9401ce374eb7492e64310837fef02b2ba5d
Cesar Quisquinay <augustoacual@gmail.com>
2024-08-30 18:01:25 -0600

    Payment Calculator Logic

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -16,16 +16,18 @@
       <Box>
         <template #header> Monthly Payment </template>
         <div>
-          <label class="label">Interest rate (2.5%)</label>
+          <label class="label">Interest rate ({{ interestRate }}%)</label>
           <input
+            v-model.number="interestRate"
             type="range"
             min="0.1"
             max="30"
             step="0.1"
             class="w-full h-4 bg-gray-200 rounded-lg apperance-none cursor-pointer dark:bg-gray-700"
           />
-          <label class="label">Duration (25 years)</label>
+          <label class="label">Duration ({{ duration }} years)</label>
           <input
+            v-model.number="duration"
             type="range"
             min="3"
             max="35"
@@ -35,7 +37,7 @@
 
           <div class="text-gray-600 dark:text-gray-300 mt-2">
             <div class="text-gray-400">Your monthly payment</div>
-            <Price :price="500" class="text-3xl" />
+            <Price :price="monthlyPayment" class="text-3xl" />
           </div>
         </div>
       </Box>
@@ -49,7 +51,25 @@ import ListingSpace from '@/components/ListingSpace.vue'
 import Price from '@/components/Price.vue'
 import Box from '@/components/UI/Box.vue'
 
-defineProps({
+import { ref, computed } from 'vue'
+
+const interestRate = ref(2.5)
+const duration = ref(25)
+
+const props = defineProps({
   listing: Object,
+})
+
+const monthlyPayment = computed(() => {
+  const principle = props.listing.price
+  const monthlyInterest = interestRate.value / 100 / 12
+  const numberOfPaymentMonths = duration.value * 12
+
+  return (
+    (principle *
+      monthlyInterest *
+      Math.pow(1 + monthlyInterest, numberOfPaymentMonths)) /
+    (Math.pow(1 + monthlyInterest, numberOfPaymentMonths) - 1)
+  )
 })
 </script>

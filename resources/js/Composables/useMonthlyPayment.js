308346ea4299d272354f23de891d7442456342de
Cesar Quisquinay <augustoacual@gmail.com>
2024-08-30 21:59:51 -0600

    Adding More Composable Members Total Paid and Total Interest

--- resources/js/Composables/useMonthlyPayment.js
+++ resources/js/Composables/useMonthlyPayment.js
@@ -16,5 +16,15 @@ export const useMonthlyPayment = (total, interestRate, duration) => {
     )
   })
 
-  return { monthlyPayment }
+  const totalPaid = computed(() => {
+    return (
+      (isRef(duration) ? duration.value : duration) * 12 * monthlyPayment.value
+    )
+  })
+
+  const totalInterest = computed(
+    () => totalPaid.value - (isRef(total) ? total.value : total)
+  )
+
+  return { monthlyPayment, totalPaid, totalInterest }
 }

5c8988e5851102d86d83c44bc04512b1f2b338a0
Cesar Quisquinay <augustoacual@gmail.com>
2024-10-22 17:01:44 -0600

    Display error in login form

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -2,9 +2,11 @@
   <form @submit.prevent="login">
     <div class="w-1/2 mx-auto">
       <div>
-        <label for="email" class="label">Email (username)</label>
+        <label for="email" class="label">Email</label>
         <input type="text" v-model="form.email" id="email" class="input" />
-        <div class="input-error">Potentials errors</div>
+        <div class="input-error" v-if="form.errors.email">
+          {{ form.errors.email }}
+        </div>
       </div>
       <div class="mt-4">
         <label for="password" class="label">Password</label>
@@ -14,6 +16,9 @@
           id="password"
           class="input"
         />
+        <div class="input-error" v-if="form.errors.password">
+          {{ form.errors.password }}
+        </div>
       </div>
       <div class="mt-4">
         <button class="btn-primary w-full" type="submit">Login</button>

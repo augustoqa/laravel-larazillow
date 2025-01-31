16a901f456b4a3a93d156cb69c05d0849c54f6b6
Augusto Quisquinay <augustoacual@gmail.com>
2025-01-30 20:05:00 -0600

    Soft Deleting Listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -110,15 +110,4 @@ class ListingController extends Controller
             ->route('listing.index')
             ->with('success', 'Listing was changed!');
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Listing $listing)
-    {
-        $listing->delete();
-
-        return redirect()->back()
-            ->with('success', 'Listing was deleted!');
-    }
 }

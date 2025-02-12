d9429b6b87f06e8118021e0c87264f930ffa3808
Augusto Quisquinay <augustoacual@gmail.com>
2025-02-12 16:19:24 -0600

    Vue Watchers Filtering Using Checkboxes

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -3,8 +3,9 @@
     <div class="mb-4 flex flex-wrap gap-2">
       <div class="flex flex-nowrap items-center gap-2">
         <input
-          type="checkbox"
+          v-model="filterForm.deleted"
           id="deleted"
+          type="checkbox"
           class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
         />
         <label for="deleted">Deleted</label>
@@ -12,3 +13,20 @@
     </div>
   </form>
 </template>
+
+<script setup>
+import { reactive, watch } from 'vue'
+import { router } from '@inertiajs/vue3'
+
+const filterForm = reactive({
+  deleted: false,
+})
+
+// reactive / ref / computed
+watch(filterForm, () =>
+  router.get(route('realtor.listing.index'), filterForm, {
+    preserveState: true,
+    preserveScroll: true,
+  })
+)
+</script>

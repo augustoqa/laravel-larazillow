6b026bb0915a38868edada0ab8a98ad7c5577f48
Augustoa Acual <augustoacual@gmail.com>
2025-11-20 17:05:27 -0600

    Sending Email - Registered user Email Verification Process

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserAccountController extends Controller
 {
-    public function create() {
+    public function create()
+    {
         return inertia('UserAccount/Create');
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $user = User::create($request->validate([
             'name' => 'required',
             'name' => 'required',
@@ -23,6 +26,7 @@ class UserAccountController extends Controller
 
         // Authenticate User
         Auth::login($user);
+        event(new Registered($user));
 
         return redirect()->route('listing.index')->with('success', 'Account created!');
     }
